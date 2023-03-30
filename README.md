81ab9ce04e10bc99a983df963de70ea476308e02
scarfacedotcom <peteradaaku@gmail.com>
2023-03-30 20:41:18 +0100

    scripts

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Sample Hardhat Project
+# Launch Pad Smart contract
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+This smart contract provides a launch pad for users to exchange Token A(Native Token) for Token B(Scar Face Token). The exchange rate is determined by the percentage of Token B that corresponds to the amount of Token A deposited by the user.
 
 Try running some of the following tasks:
 
@@ -11,3 +11,6 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.ts
 ```
+
+## Contract deployed to: 0xd19E5F27D8a122588525160c4776b7350E52C68A
+## Scar Face Token Address: 0x5886F287C4473Ce13c58474a261b31c881f8635d
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,10 +5,10 @@ async function main() {
   const [deployer, user] = await ethers.getSigners();
 
   // Deploy the contract
-  const tokenA = 0xFfb99f4A02712C909d8F7cC44e67C87Ea1E71E83;
-  const tokenB = 0x5886F287C4473Ce13c58474a261b31c881f8635d;
+  const tokenA = "0xFfb99f4A02712C909d8F7cC44e67C87Ea1E71E83";
+  const tokenB = "0x5886F287C4473Ce13c58474a261b31c881f8635d";
   const startTime = Math.floor(Date.now() / 1000);
-  const endTime = startTime + 604,800; // One hour from now
+  const endTime = startTime + 604_800; // One hour from now
 
   const launchPadFactory = await ethers.getContractFactory("LaunchPad", deployer);
   const launchPad = (await launchPadFactory.deploy(tokenA, tokenB, startTime, endTime)) as LaunchPad;
@@ -16,7 +16,7 @@ async function main() {
 
   console.log("Contract deployed to:", launchPad.address);
 
-  // Make a deposit
+  // // Make a deposit
   const depositAmount = ethers.utils.parseEther("1"); // Deposit 1 ETH
   const tokenAContract = await ethers.getContractAt("IERC20", tokenA);
   const balanceBefore = await tokenAContract.balanceOf(user.address);
@@ -32,27 +32,27 @@ async function main() {
   console.log(`Token A balance before deposit: ${ethers.utils.formatEther(balanceBefore)}`);
   console.log(`Token A balance after deposit: ${ethers.utils.formatEther(balanceAfter)}`);
 
-  // Wait until the launch pad has ended
-  const launchPadDuration = endTime - startTime;
-  await ethers.provider.send("evm_increaseTime", [launchPadDuration + 1]); // Move past end time
-  await ethers.provider.send("evm_mine", []); // Mine a new block to update the timestamp
+  // // Wait until the launch pad has ended
+  // const launchPadDuration = endTime - startTime;
+  // await ethers.provider.send("evm_increaseTime", [launchPadDuration + 1]); // Move past end time
+  // await ethers.provider.send("evm_mine", []); // Mine a new block to update the timestamp
 
-  // Withdraw tokens
-  const allocationBefore = await launchPad.allocations(user.address);
-  await launchPad.connect(user).withdraw();
-  const allocationAfter = await launchPad.allocations(user.address);
+  // // Withdraw tokens
+  // const allocationBefore = await launchPad.allocations(user.address);
+  // await launchPad.connect(user).withdraw();
+  // const allocationAfter = await launchPad.allocations(user.address);
 
-  console.log(`Withdrew ${allocationAfter.sub(allocationBefore)} Token B`);
+  // console.log(`Withdrew ${allocationAfter.sub(allocationBefore)} Token B`);
 
-  // Emergency withdraw (only owner can call this function)
-  const owner = deployer;
-  const balanceBefore2 = await ethers.provider.getBalance(owner.address);
-  await launchPad.connect(owner).emergencyWithdraw();
-  const balanceAfter2 = await ethers.provider.getBalance(owner.address);
+  // // Emergency withdraw (only owner can call this function)
+  // const owner = deployer;
+  // const balanceBefore2 = await ethers.provider.getBalance(owner.address);
+  // await launchPad.connect(owner).emergencyWithdraw();
+  // const balanceAfter2 = await ethers.provider.getBalance(owner.address);
 
-  console.log(`Emergency withdrew all Ether to ${owner.address}`);
-  console.log(`Balance before: ${ethers.utils.formatEther(balanceBefore2)}`);
-  console.log(`Balance after: ${ethers.utils.formatEther(balanceAfter2)}`);
+  // console.log(`Emergency withdrew all Ether to ${owner.address}`);
+  // console.log(`Balance before: ${ethers.utils.formatEther(balanceBefore2)}`);
+  // console.log(`Balance after: ${ethers.utils.formatEther(balanceAfter2)}`);
 }
 
 main().catch((error) => {
