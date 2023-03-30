119e9d9cc018bda2a6dc60a85116d897e89c0fa1
scarfacedotcom <peteradaaku@gmail.com>
2023-03-30 18:46:00 +0100

    launchpad contract

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,34 +1,66 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "./IERC20.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+contract LaunchPad {
+    // Token A and Token B
+    address public tokenA;
+    address public tokenB;
 
-    event Withdrawal(uint amount, uint when);
+    // Total number of Scar Face Tokens to be distributed
+    uint public constant totalTokens = 2_000_000 * 10 ** 18; // 2 million tokens
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+    // Launch pad start and end time
+    uint public startTime;
+    uint public endTime;
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    // Mapping of user's deposited Ether and allocated Token B
+    mapping(address => uint) public deposits;
+    mapping(address => uint) public allocations;
+
+    // Contract owner
+    address public owner;
+
+    constructor(address _tokenA, address _tokenB, uint _startTime, uint _endTime) {
+        tokenA = _tokenA;
+        tokenB = _tokenB;
+        startTime = _startTime;
+        endTime = _endTime;
+        owner = msg.sender;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    // Deposit Token A to receive Token B
+    function deposit(uint amount) external {
+        require(block.timestamp >= startTime && block.timestamp <= endTime, "Launch pad not active");
+        require(amount > 0, "Amount must be greater than 0");
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+        // Transfer Token A from user to contract
+        require(IERC20(tokenA).transferFrom(msg.sender, address(this), amount), "Token A transfer failed");
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+        // Calculate user's allocation of Token B
+        uint allocation = (amount * totalTokens) / IERC20(tokenA).totalSupply();
+        allocations[msg.sender] += allocation;
+        deposits[msg.sender] += amount;
+    }
 
-        owner.transfer(address(this).balance);
+    // Withdraw allocated Token B
+    function withdraw() external {
+        require(block.timestamp > endTime, "Launch pad still active");
+        require(allocations[msg.sender] > 0, "No allocation to withdraw");
+
+        // Transfer Token B from contract to user
+        require(IERC20(tokenB).transfer(msg.sender, allocations[msg.sender]), "Token B transfer failed");
+        allocations[msg.sender] = 0;
+    }
+
+    // Emergency function to withdraw all deposited Ether
+    function emergencyWithdraw() external {
+        require(msg.sender == owner, "Only owner can call this function");
+
+        // Transfer all deposited Ether from contract to owner
+        payable(owner).transfer(address(this).balance);
     }
 }
+
+
